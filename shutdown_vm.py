701a416a83d7b0085487a4f310efff83189f98eb
smallfish01 <xiaoyu712@hotmail.com>
2021-10-09 11:14:31 +0800

    Update shutdown_vm.py

--- shutdown_vm.py
+++ shutdown_vm.py
@@ -15,7 +15,7 @@ import time
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-host = '192.168.13.182'
+host = 'XX.XX.XX.XX'
 
 
 class Vcenter():
@@ -26,7 +26,7 @@ class Vcenter():
         payload = {}
         files = {}
         headers = {
-            'Authorization': 'Basic YWRtaW5pc3RyYXRvckB2c3BoZXJlLmxvY2FsOlRjMTIzNDU2Kigp'
+            'Authorization': 'Basic YWRtaW5pc3RyY234XRvckB2sdfac3BoZXJlLsafsmxvY2FsOlRjMTIzNDU2Kigpsdafsd23423sadfsdafsd234'
         }
 
         response = requests.request("POST", url, headers=headers, verify=False, data=payload, files=files)
